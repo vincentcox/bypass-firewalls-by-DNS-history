358c1a3aaf9b976802cb34126c8421e461baffa6
Vincent Cox <info@vincentcox.com>
2017-12-14 10:41:23 +0100

    improvements

--- dns-history-find-server.sh
+++ dns-history-find-server.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 #Fucked together by Vincent Cox
 domain="$1"
-list_ips=$( sources/dnshistory.org.sh "$1" )
-list_ips=$list_ips$( sources/securitytrails.com.sh "$1" )
-#---- Config ----
-protocol="https"
-#----------------
+list_ips=$( sources/dnshistory.org.sh "$domain" )
+list_ips=$list_ips$( sources/securitytrails.com.sh "$domain" )
+list_ips=$(echo $list_ips | uniq )
+result_ips=""
 for ip in $list_ips;do
-if (curl --fail --silent -k -H "Host: $domain" "$protocol"://"$ip"/ | grep "html" | grep -v "was rejected" ); then echo "$ip"; echo -e "------------\n"; fi &
+protocol="https"
+if (curl --fail --silent -k -H "Host: $domain" "$protocol"://"$ip"/ | grep "html" | grep -q -v "was rejected" ); then result_ips=$result_ips"$ip"; fi &
+protocol="http"
+if (curl --fail --silent -k -H "Host: $domain" "$protocol"://"$ip"/ | grep "html" | grep -q -v "was rejected" ); then echo "$ip"; result_ips=$result_ips"$ip"; fi &
 done
+echo $result_ips | uniq
+echo "Script done, press enter or CTRL+C to exit"
+echo "Found IP's replying to the domain: "
