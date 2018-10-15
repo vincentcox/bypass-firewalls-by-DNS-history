f921cfab23e94fc28af2da66028373f8daf9727c
Vincent Cox <info@vincentcox.com>
2018-10-15 18:25:32 +0200

    Update securitytrails.com.sh

--- sources/securitytrails.com.sh
+++ sources/securitytrails.com.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 domain=$1
-curl -s "https://app.securitytrails.com/api/domain/history/$domain/a" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+curl -s "https://securitytrails.com/domain/$domain/history/a" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
